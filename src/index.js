53f13628cfdf0fa9882a26e8869242806965818d
AKSHIT SINGH <akshitsingh2352003@gmail.com>
2026-01-29 16:31:42 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-
-ReactDOM.render(<App/>, document.querySelector("#root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
